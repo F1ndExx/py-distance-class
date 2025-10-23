074af1662bdc34f6619c94be79bcf2ecf00aad0c
Yaroslav Korotenko <Kor1k13002@gmail.com>
2025-10-23 17:03:05 +0300

    test5

--- app/main.py
+++ app/main.py
@@ -16,47 +16,52 @@ class Distance:
         return f"Distance(km={self.km})"
 
     @staticmethod
-    def _get_km(value: Union["Distance", Number]
-                ) -> Union[float, type(NotImplemented)]:
+    def _get_km(
+        value: Union["Distance", Number]
+    ) -> Union[float, type(NotImplemented)]:
         if isinstance(value, Distance):
             return value.km
         if isinstance(value, (int, float)):
             return float(value)
         return NotImplemented
 
-    def __add__(self, other: Union["Distance", Number]
-               ) -> Union["Distance", type(NotImplemented)]:
+    def __add__(
+        self, other: Union["Distance", Number]
+    ) -> Union["Distance", type(NotImplemented)]:
         km = self._get_km(other)
         if km is NotImplemented:
             return NotImplemented
         return Distance(self.km + km)
 
-    def __radd__(self, other: Union["Distance", Number]
-                ) -> Union["Distance", type(NotImplemented)]:
+    def __radd__(
+        self, other: Union["Distance", Number]
+    ) -> Union["Distance", type(NotImplemented)]:
         return self + other
 
-    def __iadd__(self, other: Union["Distance", Number]
-                ) -> Union["Distance", type(NotImplemented)]:
+    def __iadd__(
+        self, other: Union["Distance", Number]
+    ) -> Union["Distance", type(NotImplemented)]:
         km = self._get_km(other)
         if km is NotImplemented:
             return NotImplemented
         self.km += km
         return self
 
-    def __mul__(self, other: Number
-               ) -> Union["Distance", type(NotImplemented)]:
+    def __mul__(
+        self, other: Number
+    ) -> Union["Distance", type(NotImplemented)]:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         if isinstance(other, Distance):
             raise TypeError("Multiplication by Distance is not supported")
         return NotImplemented
 
-    def __rmul__(self, other: Number
-                ) -> Union["Distance", type(NotImplemented)]:
+    def __rmul__(self, other: Number) -> Union["Distance", type(NotImplemented)]:
         return self * other
 
-    def __truediv__(self, other: Number
-                   ) -> Union["Distance", type(NotImplemented)]:
+    def __truediv__(
+        self, other: Number
+    ) -> Union["Distance", type(NotImplemented)]:
         if isinstance(other, (int, float)):
             if other == 0:
                 raise ZeroDivisionError("division by zero")
